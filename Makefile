27d73d5598820593894fb653da7a4a245b3bf854
Amos Wenger <amoswenger@gmail.com>
2009-06-25 17:17:21 +0000

    Added strip to the gcj-using Makefile

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 all:
 	ant
 	cd utils/ && gcj -static-libgcj -g -Dooc.version="`cat version.txt`, built on `date -R`" `find build/ -name "*.class"` --main=org.ooc.compiler.CommandLineInterface -o ../bin/ooc
+	strip bin/ooc*
 	#cd utils/ && gcj `find ../../ubi/build/classes -name "*.class"` `find ../build/classes -name "*.class"` --main=org.ooc.compiler.CommandLineInterface -o ../dist/ooc
 
 clean:
--- src/org/ooc/nodes/types/Type.java
+++ src/org/ooc/nodes/types/Type.java
@@ -9,7 +9,6 @@ import org.ooc.errors.SourceContext;
 import org.ooc.nodes.array.ArrayDecl;
 import org.ooc.nodes.array.Subscript;
 import org.ooc.nodes.clazz.ClassDef;
-import org.ooc.nodes.clazz.Cover;
 import org.ooc.nodes.operators.Star;
 import org.ooc.nodes.others.Name;
 import org.ooc.nodes.others.SyntaxNode;
@@ -19,7 +18,6 @@ import org.ooc.structures.Clazz;
 import org.ooc.structures.Variable;
 import org.ubi.FileLocation;
 import org.ubi.SourceReader;
-import org.ubi.SyntaxError;
 
 /**
  * The usage of a type in the code, e.g. "int" or "MyClass"
