fasterthanlime/jooc-legacy

commit 6725e7d5c73dc4af32f77ba4a5c1db378d0df1ce

Keep module information for FunctionCalls on variables and use them to construct the full variable name. Fixes #62.

src/org/ooc/frontend/model/FunctionCall.java
@@ -603,6 +603,8 @@ public class FunctionCall extends Access implements MustBeResolved {
 						FuncType funcType = (FuncType) varDecl.getType();
 						if(matchesArgs(funcType.getDecl())) {
 							impl = funcType.getDecl();
+							// copy the module information for getFullName.
+							impl.module = varDecl.module;
 							break;
 						}
 					}
@@ -617,6 +619,8 @@ public class FunctionCall extends Access implements MustBeResolved {
 					if(varDecl.getType() instanceof FuncType) {
 						FuncType funcType = (FuncType) varDecl.getType();
 						impl = funcType.getDecl();
+						// copy the module information for getFullName.
+						impl.module = varDecl.module;
 					} else {
 						if(varDecl.getType() == null) return Response.OK;
 						if(fatal) {
@@ -767,5 +771,11 @@ public class FunctionCall extends Access implements MustBeResolved {
 	public boolean canBeReferenced() {
 		return false;
 	}
-	
+
+	public String getFullName() {
+		if(impl.module != null)
+			return impl.module.getMemberPrefix() + getName();
+		else
+			return getName();
+	}
 }

src/org/ooc/backend/cdirty/CallWriter.java
@@ -62,7 +62,7 @@ public class CallWriter {
 			}
 			if(impl.getSuffix().length() > 0) cgen.current.app('_').app(impl.getSuffix());
 		} else if(impl.isFromPointer()) {
-			cgen.current.app(functionCall.getName());
+			cgen.current.app(functionCall.getFullName());
 		} else {
 			impl.writeFullName(cgen.current);
 		}

commit e5720e15ef585411398e1d840bf8dc413b9d0809

FunctionCall: Only keep the module if it's a global variable declaration.
This fixes a bug introduced in 6725e7d5c73dc4af32f77ba4a5c1db378d0df1ce which
would generate wrong function call code for non-global function pointer variable calls.

src/org/ooc/frontend/model/FunctionCall.java
@@ -603,8 +603,9 @@ public class FunctionCall extends Access implements MustBeResolved {
 						FuncType funcType = (FuncType) varDecl.getType();
 						if(matchesArgs(funcType.getDecl())) {
 							impl = funcType.getDecl();
-							// copy the module information for getFullName.
-							impl.module = varDecl.module;
+							// copy the module information for getFullName if it's global.
+							if(varDecl.isGlobal())
+								impl.module = varDecl.module;
 							break;
 						}
 					}
@@ -619,8 +620,9 @@ public class FunctionCall extends Access implements MustBeResolved {
 					if(varDecl.getType() instanceof FuncType) {
 						FuncType funcType = (FuncType) varDecl.getType();
 						impl = funcType.getDecl();
-						// copy the module information for getFullName.
-						impl.module = varDecl.module;
+						// copy the module information for getFullName if it's global.
+						if(varDecl.isGlobal())
+							impl.module = varDecl.module;
 					} else {
 						if(varDecl.getType() == null) return Response.OK;
 						if(fatal) {